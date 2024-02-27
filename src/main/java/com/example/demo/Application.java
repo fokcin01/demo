bce233613fb31cd94d89f68c26d8d347d436383c
izgarshev <izgarshev_p@mail.ru>
2024-02-28 00:18:59 +0300

    вроде поломал

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,22 +1,20 @@
 package com.example.demo;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javax.swing.*;
 
-import java.io.IOException;
-
-public class Application extends javafx.application.Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+public class Application implements Runnable {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Application());
     }
 
-    public static void main(String[] args) {
-        launch();
+    @Override
+    public void run() {
+        JFrame frame = new JFrame("hueta ebanaya");
+//        JFrame frame = new Controller().getMain();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+//        JButton button = new JButton("polnyi pizdec");
+        frame.getContentPane().add(new Controller().getMain());
+        frame.setVisible(true);
     }
 }
--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,44 +1,68 @@
 package com.example.demo;
 
 import client.to.ResourceTO;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import javax.swing.*;
+import javax.swing.table.TableColumn;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 
 
 public class Controller {
-    @FXML
-    private TableView<ResourceTO> tableView;
-    @FXML
-    private TableColumn<ResourceTO, Integer> idColumn;
-    @FXML
-    private TableColumn<ResourceTO, String> nameColumn;
-    @FXML
-    private TableColumn<ResourceTO, Integer> priceColumn;
-    ResourceTO firstResource = new ResourceTO(1, "chukcha", 25);
-    ResourceTO secondResource = new ResourceTO(2, "churka", 35);
+    JPanel main = new JPanel();
 
-    public void initialize() {
-        ObservableList<ResourceTO> pohui = FXCollections.observableArrayList(firstResource, secondResource);
-        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
-        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
-        tableView.setItems(pohui);
+    public JPanel getMain() {
+        return main;
+    }
+
+    public void initialize() throws IOException, InterruptedException {
+        HttpRequest req = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/api/resources/all")).GET().build();
+        HttpResponse resp = HttpClient.newHttpClient().send(req,HttpResponse.BodyHandlers.ofString());
+        System.out.println("body: " + resp.body());
+        TypeReference<List<ResourceTO>> typeReference = new TypeReference<>() {
+        };
+        ObjectMapper mapper = new ObjectMapper();
+        List<ResourceTO> list = mapper.readValue(resp.body().toString(), typeReference);
+        System.out.println("result: " + list);
+        initTable();
+    }
+
+    Object[][] data = {
+            {"Kathy", "Smith",
+                    "Snowboarding", new Integer(5), new Boolean(false)},
+            {"John", "Doe",
+                    "Rowing", new Integer(3), new Boolean(true)},
+            {"Sue", "Black",
+                    "Knitting", new Integer(2), new Boolean(false)},
+            {"Jane", "White",
+                    "Speed reading", new Integer(20), new Boolean(true)},
+            {"Joe", "Brown",
+                    "Pool", new Integer(10), new Boolean(false)}
+    };
+    String[] columnNames = {"First Name",
+            "Last Name",
+            "Sport",
+            "# of Years",
+            "Vegetarian"};
+
+    public void initTable() {
+        JTable table = new JTable(data, columnNames);
+//        TableColumn column1 = new TableColumn();
+//        TableColumn column2 = new TableColumn();
+//        table.addColumn(column1);
+//        table.addColumn(column2);
+        main.add(table);
     }
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        HttpRequest req = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/api/resources/all")).GET().build();
-        HttpResponse resp = HttpClient.newHttpClient().send(req,HttpResponse.BodyHandlers.ofString());
-        System.out.println(resp.body());
-        Object body = resp.body();
+
+
+
     }
 }
